b8966adc8a9fc119c15e2c8f8d63297e41c93bfc
triptisingla <triptisingla306@gmail.com>
2024-03-16 17:06:47 +0530

    backend changes in jwt

--- Backend/utils/jwtToken.js
+++ Backend/utils/jwtToken.js
@@ -4,6 +4,7 @@ export const sendToken = (user, statusCode, res, message) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
+        httpOnly: true,
     };
     res.status(statusCode).cookie('token', token, options).json({
         success: true,
